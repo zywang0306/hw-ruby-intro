b24144d3371596e2c83db3a1a70aac99a814e7ff
zywang0306 <43312614+zywang0306@users.noreply.github.com>
2019-02-06 03:10:10 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,74 @@
-# When done, submit this entire file to the autograder.
-
-# Part 1
-
-def sum arr
-  # YOUR CODE HERE
+def sum arr 
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.length == 0
+  return arr[0] if arr.length == 1
+  arr.sort!{|x,y| y<=>x}
+  return arr[0]+arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  p1 = 0
+  p2 = 0
+  arr.each do |x| 
+    p2 = 0
+    arr.each do |y|  
+      if x+y == n and p1 != p2
+        return true
+      end
+      p2=p2+1
+    end
+  p1=p1+1
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, '+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  regex=/^[[a-z]&&[^aeiou]]/i
+  if  regex.match(s)
+   return true
+  
+  else
+    return false
+  end
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =="0"
+    return true
+  end
+  a = s.to_i(2)
+  
+  if a == 0
+    return false
+  end
+  if a%4 == 0
+    return true
+  else
+    return false
+  end
+
 end
 
 # Part 3
-
 class BookInStock
-# YOUR CODE HERE
+attr_accessor :isbn, :price  
+  def initialize isbn, price
+    raise ArgumentError if (isbn.empty? || price <= 0)
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    format("$%.2f",@price)
+  end
 end
